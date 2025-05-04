564b65f793d01bd63f516c0695d9867fbaffe254
Barriguete Hector <50279990+BarrigueteHector@users.noreply.github.com>
2025-05-03 21:19:40 -0600

    Create WriteYourFirstCode.cs

---
+++ WriteYourFirstCode.cs
@@ -0,0 +1,21 @@
+// Hay un salto de linea al final
+Console.WriteLine("Hello, World!");
+
+// Se imprime en una sola línea
+Console.Write("Congratualtions!");
+Console.Write(" ");
+Console.Write("You crote your first lines of code");
+
+string firstName;
+firstName = "John";
+Console.WriteLine(firstName);
+
+// var: usado para declarar variables de forma implícita, el compilador infiere el tipo de la variable, no se puede cambiar el tipo de la variable después de la declaración
+var message = "Hello World!";
+Console.WriteLine(message);
+
+string name = "Bob";
+int n = 3;
+float f = 34.4F;
+
+Console.WriteLine("Hello " + name + "! You have " + n + "messages in your inbox. The temperature is " + f + " celsius");
