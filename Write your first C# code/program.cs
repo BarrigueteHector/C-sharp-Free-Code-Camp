3c627eed6fb2a30f40f7e4cc52f01f474e23dd85
Barriguete Hector <50279990+BarrigueteHector@users.noreply.github.com>
2025-05-07 12:17:38 -0600

    Create program.cs

---
+++ Write your first C# code/program.cs
@@ -0,0 +1,11 @@
+// Hay un salto de linea al final
+Console.WriteLine("Hello, World!");
+
+// Se imprime en una sola línea
+Console.Write("Congratualtions!");
+Console.Write(" ");
+Console.Write("You wrote your first lines of code");
+
+// Ejercicio
+Console.WriteLine("This is the first line");
+Console.WriteLine("This is the second line");
